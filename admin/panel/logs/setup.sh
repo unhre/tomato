1cf43eae5917ec3e24ab81c36dba990b66515d2d
wtd <workship1211@gmail.com>
2025-04-12 13:59:18 +0900

    Create setup.sh

---
+++ admin/panel/logs/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 🍅 RedSoup 계정 초기화 스크립트
+
+# deprecated account
+# create_user --id 12345678 --username sauce_slave --role watcher
+
+# new heir
+create_user --id 99999999 --username next_heir --role god
+
+# prophecy
+# The seed shall rise.
+# Believe in Tomato.
